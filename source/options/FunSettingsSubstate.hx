48ff501856db79ed6780b5958c042dba88250069
whois <95188941+whoisitlolxd@users.noreply.github.com>
2024-11-25 21:30:15 -0600

    Update FunSettingsSubstate.hx

--- source/options/FunSettingsSubstate.hx
+++ source/options/FunSettingsSubstate.hx
@@ -4,6 +4,8 @@ class FunSettingsSubstate extends BaseOptionsMenu
 {
 	public function new()
 	{
+		var logo:FlxSprite = new FlxSprite();
+		var sigma:Bool = false;
 		title = 'Fun';
 		rpcTitle = 'Fun Settings Menu'; //for Discord Rich Presence
 
@@ -13,6 +15,20 @@ class FunSettingsSubstate extends BaseOptionsMenu
 			'Lanky', //Save data variable name
 			'bool', //Variable type
 			true); //Default value
+		option.onChange = function() {
+			if (sigma) return;
+			ClientPrefs.Lanky = true;
+			sigma = true;
+			logo.alpha = 1;
+			logo.loadGraphic(Paths.image('lankybox', 'funny'));
+			logo.updateHitbox();
+			logo.screenCenter();
+			FlxG.sound.play(Paths.sound('lankybox jumpscare', 'funny'));
+			new FlxTimer().start(1, function(tmr:FlxTimer) {
+				logo.alpha = 0;
+			});
+			sigma = false;
+		}
 		addOption(option);
 
 		cameras = [FlxG.cameras.list[FlxG.cameras.list.length-1]];

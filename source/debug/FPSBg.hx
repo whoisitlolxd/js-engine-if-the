2d017e12c6ab92a2f609dbb75dbe62f8b0331aa4
whois <95188941+whoisitlolxd@users.noreply.github.com>
2024-12-04 19:15:28 -0400

    Create FPSBg.hx

---
+++ source/debug/FPSBg.hx
@@ -0,0 +1,33 @@
+// sorry hrk i might ask next time...
+// i will remove on request, i just thought this looked nice.
+
+package debug;
+
+import openfl.events.EventType;
+import openfl.display.Sprite;
+
+class FPSBg extends Sprite
+{
+	var bgCard:Sprite;
+    var isShow:Bool = false;
+    public function new()
+    {
+        super();
+
+		bgCard = new Sprite();
+		bgCard.graphics.beginFill(0x000000, 0.5);
+		bgCard.graphics.drawRect(0, 0, 300, 55);
+		bgCard.graphics.endFill();
+		addChild(bgCard);
+		relocate(0, 0, ClientPrefs.data.wideScreen);
+    }
+
+	public inline function relocate(X:Float, Y:Float, isWide:Bool = false) {
+		if (isWide) {
+			x = X; y = Y;
+		} else {
+			x = FlxG.game.x + X;
+			y = FlxG.game.y + Y;
+		}
+	}
+}

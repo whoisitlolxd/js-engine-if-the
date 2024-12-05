c72a9d31c259a1e2900f6003ba1028bb76b3a33d
whois <95188941+whoisitlolxd@users.noreply.github.com>
2024-12-04 20:53:26 -0400

    Update LoadingState.hx

--- source/LoadingState.hx
+++ source/LoadingState.hx
@@ -176,7 +176,7 @@ class LoadingState extends MusicBeatState
 		return target;
 	}
 	
-	#if NO_PRELOAD_ALL
+	//#if NO_PRELOAD_ALL
 	static function isSoundLoaded(path:String):Bool
 	{
 		return Assets.cache.hasSound(path);
@@ -186,7 +186,7 @@ class LoadingState extends MusicBeatState
 	{
 		return Assets.getLibrary(library) != null;
 	}
-	#end
+	//#end
 	
 	override function destroy()
 	{

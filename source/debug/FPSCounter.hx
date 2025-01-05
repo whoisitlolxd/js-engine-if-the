494b12096b818e2c2e9817b53d15f004b966b099
whois <95188941+whoisitlolxd@users.noreply.github.com>
2025-01-05 19:51:40 -0400

    Update FPSCounter.hx

--- source/debug/FPSCounter.hx
+++ source/debug/FPSCounter.hx
@@ -8,31 +8,16 @@ import debug.Memory;
 
 class FPSCounter extends TextField
 {
-	var the:Bool = false;
-	var fpsBg:FPSBg = new FPSBg(); // god will this even work?
 	public var currentFPS(default, null):Float;
-
-	/*
-	inline function gay():Float
-	{
-		#if (cpp && windows)
-		return 0;
-		#elseif cpp
-		return cpp.vm.Gc.memInfo64(cpp.vm.Gc.MEM_INFO_USAGE);
-		#else
-		return cast(openfl.system.System.totalMemory, UInt);
-		#end
-	}
-	*/
 
 	/**
 		The current memory usage (WARNING: this is NOT your total program memory usage, rather it shows the garbage collector memory)
 	**/
-    public var memory(get, never):Float;
+	public var memory(get, never):Float;
 	inline function get_memory():Float
 		return Memory.gay();
 
-    var mempeak:Float = 0;
+	var mempeak:Float = 0;
 
 	@:noCompletion private var times:Array<Float>;
 
@@ -46,7 +31,7 @@ class FPSCounter extends TextField
 		currentFPS = 0;
 		selectable = false;
 		mouseEnabled = false;
-		defaultTextFormat = new TextFormat(Paths.font("comic.ttf"), 12, color); // vcr font smells okay
+		defaultTextFormat = new TextFormat("VCR OSD Mono", 12, color);
 		autoSize = LEFT;
 		multiline = true;
 		text = "FPS: ";
@@ -54,33 +39,36 @@ class FPSCounter extends TextField
 		times = [];
 	}
 
-    var timeColor:Float = 0.0;
+	var timeColor:Float = 0.0;
 
 	var fpsMultiplier:Float = 1.0;
-    var deltaTimeout:Float = 0.0;
+	var deltaTimeout:Float = 0.0;
+	public var timeoutDelay:Float = 50;
+	var now:Float = 0;
 	// Event Handlers
 	override function __enterFrame(deltaTime:Float):Void
 	{
-		if (deltaTimeout > 1000) {
-			deltaTimeout = 0.0;
+		if (!ClientPrefs.showFPS) return;
+		now = haxe.Timer.stamp() * 1000;
+		times.push(now);
+		while (times[0] < now - 1000 / fpsMultiplier) times.shift();
+		if (deltaTimeout <= timeoutDelay)
+		{
+			deltaTimeout += deltaTime;
 			return;
 		}
 
-		final now:Float = haxe.Timer.stamp() * 1000;
-		times.push(now);
-		while (times[0] < now - 1000 / fpsMultiplier) times.shift();
-
-		if (Std.isOfType(FlxG.state, PlayState) && !PlayState.instance.trollingMode) { 
+		if (Std.isOfType(FlxG.state, PlayState) && !PlayState.instance.trollingMode)
+		{
 			try { fpsMultiplier = PlayState.instance.playbackRate; }
 			catch (e:Dynamic) { fpsMultiplier = 1.0; }
 		}
 		else fpsMultiplier = 1.0;
 
-        if (memory > mempeak) mempeak = memory;
+		if (memory > mempeak) mempeak = memory;
 
-        currentFPS = Math.min(FlxG.drawFramerate, times.length) / fpsMultiplier;
-        updateText();
-        deltaTimeout += deltaTime;
+		currentFPS = Math.min(FlxG.drawFramerate, times.length) / fpsMultiplier;
+		updateText();
 
 		if (ClientPrefs.rainbowFPS)
 		{
@@ -95,38 +83,22 @@ class FPSCounter extends TextField
 
 			if (currentFPS <= ClientPrefs.framerate / 3 && currentFPS >= ClientPrefs.framerate / 4)
 				textColor = 0xFFFF8000;
-				
+
 			if (currentFPS <= ClientPrefs.framerate / 4)
 				textColor = 0xFFFF0000;
 		}
+		deltaTimeout = 0.0;
 	}
 
-	public dynamic function updateText():Void { // so people can override it in hscript
-		text = (ClientPrefs.showFPS ? "FPS: " + (ClientPrefs.ffmpegMode ? ClientPrefs.targetFPS : Math.round(currentFPS)) : "");
-		if (ClientPrefs.ffmpegMode) {
+	public dynamic function updateText():Void   // so people can override it in hscript
+	{
+		text = "FPS: " + (ClientPrefs.ffmpegMode ? ClientPrefs.targetFPS : Math.round(currentFPS));
+		if (ClientPrefs.ffmpegMode)
 			text += " (Rendering Mode)";
-		}
-		if (!ClientPrefs.showFPS && fpsBg.isShow)
-		{
-			fpsBg.invis();
-		}
-		else if (ClientPrefs.showFPS && !fpsBg.isShow)
-		{
-			fpsBg.vis();
-		}
-		if (!the && ClientPrefs.gugugagamode)
-		{
-			the = true;
-			setTextFormat(new TextFormat(Paths.font("PermanentMarker-Regular.ttf"), 12, textColor));
-		}
-		else if (the && !ClientPrefs.gugugagamode)
-		{
-			the = false;
-			setTextFormat(defaultTextFormat);
-		}
-		
+
 		if (ClientPrefs.showRamUsage) text += "\nRAM: " + FlxStringUtil.formatBytes(memory) + (ClientPrefs.showMaxRamUsage ? " / " + FlxStringUtil.formatBytes(mempeak) : "");
-		if (ClientPrefs.debugInfo) {
+		if (ClientPrefs.debugInfo)
+		{
 			text += '\nState: ${Type.getClassName(Type.getClass(FlxG.state))}';
 			if (FlxG.state.subState != null)
 				text += '\nSubstate: ${Type.getClassName(Type.getClass(FlxG.state.subState))}';
